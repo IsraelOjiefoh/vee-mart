b708b7d1cd0aef245ba07bca1f2cada064ea5979
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-23 03:52:01 +0100

    modified

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 import { Collapse, Dropdown, initTE } from "tw-elements";
-import { Datepicker, Input } from "tw-elements";
 /* eslint-disable react/no-unknown-property */
+
 const Navbar = () => {
   // Initialization for ES Users
-  initTE({ Datepicker, Input }, { allowReinits: true });
+
   initTE({ Collapse, Dropdown });
   return (
     <>
@@ -66,7 +66,7 @@ const Navbar = () => {
                   href="#"
                   data-te-nav-link-ref
                 >
-                  Home
+                  Dashboard
                 </a>
               </li>
               {/* <!-- Team link --> */}
@@ -76,7 +76,7 @@ const Navbar = () => {
                   href="#"
                   data-te-nav-link-ref
                 >
-                  Shop
+                  Team
                 </a>
               </li>
               {/* <!-- Projects link --> */}
@@ -86,7 +86,7 @@ const Navbar = () => {
                   href="#"
                   data-te-nav-link-ref
                 >
-                  Contact Us
+                  Projects
                 </a>
               </li>
             </ul>
@@ -111,9 +111,125 @@ const Navbar = () => {
               </span>
             </a>
 
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded">
-              Account/Login
-            </button>
+            {/* <!-- Container with two dropdown menus --> */}
+            <div
+              className="relative"
+              data-te-dropdown-ref
+              data-te-dropdown-alignment="end"
+            >
+              {/* <!-- First dropdown trigger --> */}
+              <a
+                className="hidden-arrow mr-4 flex items-center text-neutral-600 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                href="#"
+                id="dropdownMenuButton1"
+                role="button"
+                data-te-dropdown-toggle-ref
+                aria-expanded="false"
+              >
+                {/* <!-- Dropdown trigger icon --> */}
+                <span className="[&>svg]:w-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </a>
+              {/* <!-- First dropdown menu --> */}
+              <ul
+                className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+                aria-labelledby="dropdownMenuButton1"
+                data-te-dropdown-menu-ref
+              >
+                {/* <!-- First dropdown menu items --> */}
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* <!-- Second dropdown container --> */}
+            <div
+              className="relative"
+              data-te-dropdown-ref
+              data-te-dropdown-alignment="end"
+            >
+              {/* <!-- Second dropdown trigger --> */}
+              <a
+                className="hidden-arrow flex items-center whitespace-nowrap transition duration-150 ease-in-out motion-reduce:transition-none"
+                href="#"
+                id="dropdownMenuButton2"
+                role="button"
+                data-te-dropdown-toggle-ref
+                aria-expanded="false"
+              ></a>
+              {/* <!-- Second dropdown menu --> */}
+              <ul
+                className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+                aria-labelledby="dropdownMenuButton2"
+                data-te-dropdown-menu-ref
+              >
+                {/* <!-- Second dropdown menu items --> */}
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
+                    href="#"
+                    data-te-dropdown-item-ref
+                  >
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
