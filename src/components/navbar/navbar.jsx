0b9a0e88b583e41441afdbb0879ad4809ecec865
Israel Ojiefoh <israelmayowa580@gmail.com>
2023-09-24 00:25:39 +0100

    modified

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,7 +7,6 @@ function Navbar() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  
 
   return (
     <nav className="bg-blue-500 p-4">
@@ -19,24 +18,6 @@ function Navbar() {
 
         {/* Right-side navigation elements */}
         <div className="flex items-center">
-          {/* Search Bar */}
-          <div className="hidden lg:flex items-center">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search"
-                className="border rounded-lg pl-10 pr-2 py-1" // Adjust padding for icon
-              />
-              <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                <i className="fas fa-search text-gray-500 cursor-pointer"></i>{" "}
-                {/* Font Awesome search icon */}
-              </span>
-              <button className="ml-2 bg-blue-700 text-white px-2 py-1 rounded-lg">
-                Search
-              </button>
-            </div>
-          </div>
-
           {/* User Account */}
           <button className="hidden lg:block ml-4  text-white px-2 py-1 bg-gray-500 rounded-lg ">
             <a href="/login">
@@ -106,7 +87,7 @@ function Navbar() {
           </div>
         </div>
       )}
-    {/* <SearchBar  ></SearchBar> */}
+      {/* <SearchBar  ></SearchBar> */}
     </nav>
   );
 }
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,10 +1,10 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [selectedCategory, setSelectedCategory] = useState("All Products");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,7 +12,6 @@ const Products = () => {
         const response = await axios.get("https://fakestoreapi.com/products");
         const fetchedProducts = response.data;
         console.log(fetchedProducts);
-
         setProducts(fetchedProducts);
         setIsLoading(false);
       } catch (err) {
@@ -24,35 +23,75 @@ const Products = () => {
     fetchData();
   }, []);
 
-  // Filter products by category
-  const filteredData =
-    selectedCategory === "All Products"
-      ? products
-      : products.filter((item) => item.category === selectedCategory);
-
-  // Handle category selection change
-  const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value);
+  // Function to handle category selection and update the selectedCategory state
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
   };
+
+  // Filter products based on the selected category
+  const filteredProducts = selectedCategory
+    ? products.filter(
+        (product) => product.category.toLowerCase() === selectedCategory
+      )
+    : products;
 
   return (
     <div className="container mx-auto py-4">
-      <div className="lg:hidden flex items-center justify-center bg-gray-200 p-2 rounded">
-        {/* Category Select */}
-        <div className="flex items-center">
-          <select
-            value={selectedCategory}
-            onChange={handleCategoryChange}
-            className="border rounded-lg px-4 py-1"
+      <h1 className="text-2xl text-center pt-10  font-bold">Categories</h1>
+      <div className=" flex items-center justify-center bg-gray-200 p-2 rounded my-5">
+        {/* Category Navigation Buttons */}
+        <div className="flex space-x-4 my-5">
+          <button
+            onClick={() => handleCategoryChange("")}
+            className={`${
+              selectedCategory === "" ? "bg-blue-600 text-white" : "bg-gray-300"
+            } px-2 py-1 rounded`}
           >
-            <option value="All Products">All Products</option>
-            <option value="men's clothing">Men Clothing</option>
-            <option value="women's clothing">Women Clothing</option>
-            <option value="electronics">Tech</option>
-            <option value="jewelery">Jewelry</option>
-          </select>
+            All
+          </button>
+          <button
+            onClick={() => handleCategoryChange("electronics")}
+            className={`${
+              selectedCategory === "electronics"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-300"
+            } px-2 py-1 rounded`}
+          >
+            Electronics
+          </button>
+          <button
+            onClick={() => handleCategoryChange("men's clothing")}
+            className={`${
+              selectedCategory === "men's clothing"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-300"
+            } px-2 py-1 rounded`}
+          >
+            Men
+          </button>
+          <button
+            onClick={() => handleCategoryChange("women's clothing")}
+            className={`${
+              selectedCategory === "women's clothing"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-300"
+            } px-2 py-1 rounded`}
+          >
+            Women
+          </button>
+          <button
+            onClick={() => handleCategoryChange("jewelery")}
+            className={`${
+              selectedCategory === "jewelery"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-300"
+            } px-2 py-1 rounded`}
+          >
+            Jewelery
+          </button>
         </div>
       </div>
+      {/* Product Listing */}
       {isLoading ? (
         <div className="flex items-center justify-center h-40">
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
@@ -72,8 +111,8 @@ const Products = () => {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {filteredData.map((product) => (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  ">
+          {filteredProducts.map((product) => (
             <div
               key={product.id}
               className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
@@ -81,7 +120,7 @@ const Products = () => {
               <img
                 src={product.image}
                 alt={product.title}
-                className="w-full h-40 object-cover rounded-lg"
+                className="w-full h-40 object-cover rounded-lg "
               />
               <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
               <p className="text-gray-600">${product.price}</p>
